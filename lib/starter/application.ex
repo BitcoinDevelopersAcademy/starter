99ef8aaac06a64f3cf259e41b94d67115594cac8
diop <fodesdiop@gmail.com>
2022-09-07 09:48:46 -0500

    first commit

---
+++ lib/starter/application.ex
@@ -0,0 +1,36 @@
+defmodule Starter.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Start the Ecto repository
+      Starter.Repo,
+      # Start the Telemetry supervisor
+      StarterWeb.Telemetry,
+      # Start the PubSub system
+      {Phoenix.PubSub, name: Starter.PubSub},
+      # Start the Endpoint (http/https)
+      StarterWeb.Endpoint
+      # Start a worker by calling: Starter.Worker.start_link(arg)
+      # {Starter.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Starter.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  @impl true
+  def config_change(changed, _new, removed) do
+    StarterWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
